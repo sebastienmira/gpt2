2645d6f55cbec8cde37642ae9780ff1589a83bc1
sebastienmira <129765519+sebastienmira@users.noreply.github.com>
2024-12-12 22:58:11 +0000

    skeleton for gpt2

---
+++ train.py
@@ -0,0 +1,99 @@
+from dataclasses import dataclass
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+import math
+
+
+#-------------
+class CausalSelfAttention(nn.Module):
+    #Multi-headed attention 
+
+    def __init__(self, config):
+        super().__init__()
+        assert config.n_embd % config.n_head == 0
+
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd) # batch of K,Q,V projections for all heads
+
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+
+        self.register_buffer('bias', torch.tril(torch.ones(config.block_size, config.block_size))
+                             .view(1,1,config.block_size, config.block_size))
+
+    def forward(self,x):
+        B,T,C = x.size()
+        
+        qkv =self.c_attn(x)
+        q,k,v = qkv.split(self.n_embd, 2)
+        
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs) 
+        
+        #compute affinities/calculate attention
+        att = q @ k.transpose(-2,-1) * (1.0 / math.sqrt(k.size(-1)))
+        att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+        att = F.softmax(att, dim=-1)
+        y = att @ v
+        y = y.transpose(1,2).contiguous().view(B,T,C) #all head outputs side by side
+        y = self.c_proj(y)
+        return y
+
+
+class MLP(nn.Module):
+    #Linear layer + Non-linearity
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
+        self.gelu = nn.GELU(approximate = 'tanh') #gaussion error linear units. using approximation to reproduce gpt-2
+        self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        return x
+
+
+class Block(nn.Module):
+    #Transformer block: communication(attention) followed by computation (ffwd)
+
+    def __init__(self, config):
+        super().__init__()
+        self.ln1 = nn.LayerNorm(config.n_embd)
+        self.attn = CausalSelfAttention(config)
+        self.ln2 = nn.LayerNorm(config.n_embd)
+        self.mlp = MLP(config)
+
+    def forward(self, x):
+        x = x + self.attn(self.ln1(x)) #normalizing and implementing residual connections 
+        x = x + self.mlp(self.ln2(x)) #normalizing and implementing residual connections
+        return x
+        
+
+
+@dataclass 
+class GPTConfig:
+    block_size: int = 1024
+    vocab_size: int = 50257 #50000 bpe merger + 256 byte tokens + end token
+    n_layer: int = 12
+    n_head: int = 12
+    n_embd: int = 768
+
+class GPT(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+
+        self.transformer = nn.ModuleDict(dict(
+            wte = nn.Embedding(config.vocab_size, config.n_embd),
+            wpe = nn.Embedding(config.block_size, config.n_embd),
+            h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
+            ln_f = nn.LayerNorm(config.n_embd),
+        ))
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+
